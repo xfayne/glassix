ba9348e288056da6be248da338dbc926ce746983
xfayne <xfayne@gmail.com>
2021-02-18 10:30:49 +0200

    app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const IPDATA_KEY = 'dcb19aafdd3accd0ca61b014f91a6cc5888d57f4f703a9758da3751e'
+const GOOGLE_API_KEY = 'AIzaSyCdQymwSuF0P6Ee-ffX0ZtWjpJdpaT5eLk'
+
+export default function App() {
+  const [state, setState] = useState()
+
+  useEffect(()=>  {async function fetchSetting() {
+    const result = await axios.get(`https://api.ipdata.co?api-key=${IPDATA_KEY}`)
+    const {city,country_name,latitude,longitude} = await result.data
+    setState ({city,country_name,latitude,longitude})
+   } 
+   fetchSetting();}, []) 
+  
+   return state? (
+    <div style ={{backgroundColor:'#f4f3ef',height:'100vh', fontFamily:'Helvetica Neue'}}>
+      <br/>
+      <div style={{width:'75%', backgroundColor:'white', textAlign:'center', margin:'auto', border:'1px solid black'}} >
+        <h1 style={{paddingTop:'3vh'}}>Glassix Assignment - Or Fayne</h1>
+        <h2>Location - country: {state.country_name}, city: {state.city}</h2>
+        <iframe title="map" style ={{width:'90%', height:'60vh',marginBottom:'5%'}}
+        src={`https://www.google.com/maps/embed/v1/place?key=${GOOGLE_API_KEY}&q=${state.latitude},${state.longitude}`}>
+      </iframe>
+      </div>
     </div>
-  );
+  )
+  : <h1>An Error was accord</h1>
 }
 
-export default App;
